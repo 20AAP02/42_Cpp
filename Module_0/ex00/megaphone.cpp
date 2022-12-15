841c3b00c867be44148a61141158512bd00edb87
Antonio Maria mouro ferreira abranches pinto <amaria-m@c1r5s5.42lisboa.com>
2022-12-15 17:30:40 +0000

    Started ex01

--- Module_0/ex00/megaphone.cpp
+++ Module_0/ex00/megaphone.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <cctype>
 
+char *str_upper(char *str)
+{
+	for (int index = 0; str[index]; index++)
+		str[index] = (char)toupper(str[index]);
+	return str;
+}
+
 int main(int argc, char **argv)
 {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-		for (int i = 1; i < argc; i++)
-			for (int j = 0; argv[i][j] > 0; j++)
-				std::cout << (char)toupper(argv[i][j]);
+		for (int index = 1; index < argc; index++)
+			std::cout << str_upper(argv[index]);
 	std::cout << std::endl;
 	return 0;
 }
