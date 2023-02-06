1aef31fb51d1303dd8e93f8915037fdf16648afe
Antonio Maria mouro ferreira abranches pinto <antoniomariabranches@gmail.com>
2023-02-06 18:35:09 +0000

    corrected module 03

--- Module_3/ex01/main.cpp
+++ Module_3/ex01/main.cpp
@@ -1,19 +1,24 @@
-#include <ClapTrap.hpp>
 #include <ScavTrap.hpp>
 
 int main()
 {
+	std::cout << "----- Constructors -----\n";
 	ScavTrap a("Joe");
 	ScavTrap b("Richard");
 	ScavTrap c("Elizabeth");
 	ScavTrap x;
 	ScavTrap l(c);
 	
-	a.attack(b.getName());
-	b.takeDamage(a.getAttackDmg());
-	c.takeDamage(50);
-	x = a;
-	x.attack(c.getName());
-	x.guardGate();
+	std::cout << "\n----- Points -----\n";
+	std::cout << "Hit points: " << a.getHitPts() << std::endl;
+	std::cout << "Energy points: " << a.getEnergyPts() << std::endl;
+	std::cout << "Attack damage: " << a.getAttackDmg() << std::endl;
+	std::cout << "\n----- Default functions -----\n";
+	x = l;
+	a.attack(x.getName());
+	x.takeDamage(a.getAttackDmg());
+	l.beRepaired(5);
+	b.guardGate();
+	std::cout << "\n----- Deconstructors -----\n";
 	return 0;
 }
--- Module_3/ex03/main.cpp
+++ Module_3/ex03/main.cpp
@@ -2,21 +2,25 @@
 
 int main()
 {
-	std::cout << "--------- Constructors ---------\n";
+	std::cout << "----- Constructors -----\n";
 	DiamondTrap a("Joe");
-	DiamondTrap b(a);
-	DiamondTrap c;
-
-	std::cout << "--------- Member Functions ---------\n";
+	DiamondTrap b("Richard");
+	DiamondTrap c("Elizabeth");
+	DiamondTrap x;
+	DiamondTrap l(c);
+	
+	std::cout << "\n----- Points -----\n";
+	std::cout << "Hit points: " << a.getHitPts() << std::endl;
+	std::cout << "Energy points: " << a.getEnergyPts() << std::endl;
+	std::cout << "Attack damage: " << a.getAttackDmg() << std::endl;
+	std::cout << "\n----- Default functions -----\n";
+	x = l;
+	a.attack(x.getName());
+	x.takeDamage(a.getAttackDmg());
+	l.beRepaired(5);
+	b.guardGate();
+	b.highFivesGuys();
 	c.whoAmI();
-	c = a;
-	std::cout << "Attack damage: " << b.getAttackDmg() << std::endl;
-	std::cout << "Energy points: " << b.getEnergyPts() << std::endl;
-	std::cout << "Hit points: " << b.getHitPts() << std::endl;
-	a.attack(a.getName());
-	a.whoAmI();
-	b.whoAmI();
-	c.whoAmI();
-	std::cout << "--------- Deconstructors ---------\n";
+	std::cout << "\n----- Deconstructors -----\n";
 	return 0;
 }
