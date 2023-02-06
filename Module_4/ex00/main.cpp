cfa095db35f4d92e7759869b8a13be80f191f200
Antonio Maria mouro ferreira abranches pinto <antoniomariabranches@gmail.com>
2023-02-06 19:43:27 +0000

    ex01 of module 04, on going

--- Module_4/ex00/main.cpp
+++ Module_4/ex00/main.cpp
@@ -4,31 +4,43 @@
 #include <WrongAnimal.hpp>
 #include <WrongCat.hpp>
 
-// int main()
-// {
-// 	const Animal* meta = new Animal();
-// 	const Animal* j = new Dog();
-// 	const Animal* i = new Cat();
-// 	std::cout << j->getType() << " " << std::endl;
-// 	std::cout << i->getType() << " " << std::endl;
-// 	i->makeSound(); // will output the cat sound!
-// 	j->makeSound();
-// 	meta->makeSound();
-// 	delete meta;
-// 	delete j;
-// 	delete i;
-// 	return 0;
-// }
-
 int main()
 {
-	const WrongAnimal* meta = new WrongAnimal();
-	const WrongAnimal* i = new WrongCat();
-	const WrongAnimal p;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); // will output the cat sound!
-	meta->makeSound();
-	delete meta;
-	delete i;
-	return 0;
+	{
+		std::cout << "---------- Right Animals ----------\n";
+		std::cout << "-- Constructors --\n";
+		const Animal* meta = new Animal();
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		std::cout << "-- Types --\n";
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		std::cout << "-- Sounds --\n";
+		i->makeSound(); // will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
+		std::cout << "-- Deconstructors --\n";
+		delete meta;
+		delete j;
+		delete i;
+	}
+	{
+		std::cout << "\n---------- Wrong Animals ----------\n";
+		std::cout << "-- Constructors --\n";
+		const WrongAnimal* meta = new WrongAnimal();
+		const WrongAnimal *i = new WrongCat();
+		const WrongCat c;
+		std::cout << "-- Types --\n";
+		std::cout << meta->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		std::cout << "-- Sounds --\n";
+		i->makeSound(); // will output the animal sound!
+		((WrongCat *) i)->makeSound(); // will output the cat sound!
+		meta->makeSound();
+		c.makeSound();
+		std::cout << "-- Deconstructors --\n";
+		delete meta;
+		delete i;
+		return 0;
+	}
 }
