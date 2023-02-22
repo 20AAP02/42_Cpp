f33ae015afbb2ed3c69c4e10b18630fa0182fa38
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-02-22 20:43:16 +0000

    finished module 06, i might have to add error testing to ex00

--- Module_6/ex02/RealType.cpp
+++ Module_6/ex02/RealType.cpp
@@ -1,7 +1,13 @@
 #include <RealType.hpp>
 
-Base *generate(void);
+Base *generate(void)
 {
+	static int randomCheck;
+	if (randomCheck == 0)
+	{
+		srand(time(0));
+		randomCheck = 1;
+	}
 	Base * newBase;
 	int i = rand() % 3;
 	switch (i)
@@ -12,8 +18,7 @@ Base *generate(void);
 		case 1:
 			newBase = new B();
 			break;
-		
-		default:
+		case 2:
 			newBase = new C();
 			break;
 	}
@@ -22,8 +27,47 @@ Base *generate(void);
 
 void identify(Base *p)
 {
+	if (dynamic_cast<A*>(p) != NULL)
+		std::cout << "This object is of type A\n";
+	else if (dynamic_cast<B*>(p) != NULL)
+		std::cout << "This object is of type B\n";
+	else if (dynamic_cast<C*>(p) != NULL)
+		std::cout << "This object is of type C\n";
+	else
+		std::cout << "Thobject is of unknown type\n";
 }
 
 void identify(Base &p)
 {
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void) a;
+		std::cout << "This object is of type A\n";
+		return ;
+	}
+	catch(const std::exception& e)
+	{
+	}
+	try
+	{
+		B &b = dynamic_cast<B&>(p);
+		(void) b;
+		std::cout << "This object is of type B\n";
+		return ;
+	}
+	catch(const std::exception& e)
+	{
+	}
+	try
+	{
+		C &c = dynamic_cast<C&>(p);
+		(void) c;
+		std::cout << "This object is of type C\n";
+		return ;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Object is of unknown type\n";
+	}
 }
--- Module_6/ex02/main.cpp
+++ Module_6/ex02/main.cpp
@@ -2,5 +2,26 @@
 
 int main()
 {
+	Base *a = generate();
+	Base *b = generate();
+	Base *c = generate();
+	Base &d = *a;
+	Base &e = *b;
+	Base &f = *c;
+
+	std::cout << "--- Testing identify() with pointer ---\n";
+	identify(a);
+	identify(b);
+	identify(c);
+	std::cout << "--- Testing identify() with reference ---\n";
+	identify(d);
+	identify(e);
+	identify(f);
+
+	
+	delete a;
+	delete b;
+	delete c;
+
 	return 0;
 }
