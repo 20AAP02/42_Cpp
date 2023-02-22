b876a00b6bde8b410d53412902e7d9d3b02a7e10
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-02-22 17:39:26 +0000

    ex00 from module 06 is done, might still have to add error parsing

--- Module_6/ex00/Literal.cpp
+++ Module_6/ex00/Literal.cpp
@@ -6,11 +6,28 @@
 
 Literal::Literal()
 {
+	this->_char = 0;
+	this->_int = 0;
+	this->_float = 0;
+	this->_double = 0;
 }
 
 Literal::Literal(const std::string literal)
 {
-
+	this->_literal = literal;
+	this->_char = 0;
+	this->_int = 0;
+	this->_float = 0;
+	this->_double = 0;
+	this->_originalType = this->convertToOriginalType(literal);
+	if (this->_originalType == "char")
+		this->convert(this->_char);
+	else if (this->_originalType == "int")
+		this->convert(this->_int);
+	else if (this->_originalType == "float" || this->_originalType == "floatP")
+		this->convert(this->_float);
+	else if (this->_originalType == "double" || this->_originalType == "doubleP")
+		this->convert(this->_double);
 }
 
 Literal::Literal( const Literal & src )
@@ -64,7 +81,7 @@ std::string Literal::getType(const std::string arg) const
 {
 	const std::string pseudoFloats[] = {"-inff", "+inff", "nanf"};
 	const std::string pseudoDoubles[] = {"-inf", "+inf", "nan"};
-	if (std::isdigit(arg[0]))
+	if (std::isdigit(arg[0]) || (arg.length() > 1 && std::isdigit(arg[1])))
 	{
 		if (arg[arg.length() - 1] == 'f')
 			return "float";
@@ -79,6 +96,106 @@ std::string Literal::getType(const std::string arg) const
 		if (arg == pseudoDoubles[i])
 			return "doubleP";
 	return "char";
+}
+
+std::string Literal::convertToOriginalType(const std::string literal)
+{
+	std::string type = this->getType(literal);
+
+	if (type == "char")
+		this->_char = literal[0];
+	else if (type == "int")
+		this->_int = atoi(literal.c_str());
+	else if (type == "float" || type == "floatP")
+		this->_float = static_cast<float>(atof(literal.c_str()));
+	else if (type == "double" || type == "doubleP")
+		this->_double = atof(literal.c_str());
+	return type;
+}
+
+void Literal::convert(char literal)
+{
+	this->_int = static_cast<int>(literal);
+	this->_float = static_cast<float>(literal);
+	this->_double = static_cast<double>(literal);
+}
+
+void Literal::convert(int literal)
+{
+	if (literal >= MIN_CHAR && literal <= MAX_CHAR)
+		this->_char = static_cast<char>(literal);
+	this->_float = static_cast<float>(literal);
+	this->_double = static_cast<double>(literal);
+}
+
+void Literal::convert(float literal)
+{
+	if (literal >= MIN_CHAR && literal <= MAX_CHAR)
+		this->_char = static_cast<char>(literal);
+	if (literal >= MIN_INT && literal <= MAX_INT)
+		this->_int = static_cast<int>(literal);
+	this->_double = static_cast<double>(literal);
+}
+
+void Literal::convert(double literal)
+{
+	if (literal >= MIN_CHAR && literal <= MAX_CHAR)
+		this->_char = static_cast<char>(literal);
+	if (literal >= MIN_INT && literal <= MAX_INT)
+		this->_int = static_cast<int>(literal);
+	this->_float = static_cast<float>(literal);
+}
+
+void Literal::printValues(void)
+{
+	const std::string pseudoFloats[] = {"-inff", "+inff", "nanf"};
+	const std::string pseudoDoubles[] = {"-inf", "+inf", "nan"};
+	for (int i = 0; i < 3; i++)
+	{
+		if (this->_literal == pseudoFloats[i] || this->_literal == pseudoDoubles[i])
+		{
+			std::cout << "char: impossible\n";
+			std::cout << "int: impossible\n";
+			std::cout << "float: " << pseudoFloats[i] << std::endl;
+			std::cout << "double: " << pseudoDoubles[i] << std::endl;
+			return ;
+		}
+	}
+	// Char
+	std::cout << "char: ";
+	if (this->_char == 0 && (this->_double >= 1.0 || this->_double <= -1.0))
+		std::cout << "impossible\n";
+	else if (this->_char < 32 || this->_char == 127)
+		std::cout << "Non displayable\n";
+	else
+		std::cout << this->_char << std::endl;
+	std::cout << "int: ";
+	if (this->_int == 0 && (this->_double >= 1.0 || this->_double <= -1.0))
+		std::cout << "impossible\n";
+	else
+		std::cout << this->_int << std::endl;
+	std::cout << "float: " << this->_float;
+	if (!(this->_float != this->_float || this->_float < MIN_FLOAT || this->_float > MAX_FLOAT))
+	{
+		if (this->_originalType == "int" || this->_originalType == "char")
+			std::cout << ".0";
+		else if (this->_float > 0.0f && !(this->_float - (static_cast<int>(this->_float)) > 0.0f))
+			std::cout << ".0";
+		else if (this->_float < 0.0f && !((this->_float * -1.0f) - (static_cast<int>(this->_float)) > 0.0f))
+			std::cout << ".0";
+	}
+	std::cout << "f\n";
+	// Double
+	std::cout << "double: " << this->_double;
+	if (!(this->_double != this->_double || this->_double < MIN_DOUBLE || this->_double > MAX_DOUBLE))
+	{
+		if (this->_originalType == "int" || this->_originalType == "char")
+			std::cout << ".0";
+		else if (this->_double > 0.0 && !(this->_double - (static_cast<int>(this->_double)) > 0.0))
+			std::cout << ".0";
+		else if (this->_double < 0.0 && !((this->_double * -1.0) - (static_cast<int>(this->_double)) > 0.0))
+			std::cout << ".0";
+	}
 }
 
 /*
--- Module_6/ex00/main.cpp
+++ Module_6/ex00/main.cpp
@@ -1,28 +1,6 @@
 #include <iostream>
 #include <string>
-#include <iostream>
-#include <sstream>
-
-std::string getType(std::string arg)
-{
-	const std::string pseudoFloats[] = {"-inff", "+inff", "nanf"};
-	const std::string pseudoDoubles[] = {"-inf", "+inf", "nan"};
-	if (std::isdigit(arg[0]))
-	{
-		if (arg[arg.length() - 1] == 'f')
-			return "float";
-		if (arg.find(".") != arg.npos)
-			return "double";
-		return "int";
-	}
-	for (int i = 0; i < 3; i++)
-		if (arg == pseudoFloats[i])
-			return "floatP";
-	for (int i = 0; i < 3; i++)
-		if (arg == pseudoDoubles[i])
-			return "doubleP";
-	return "char";
-}
+#include <Literal.hpp>
 
 int main(int argc, char *argv[])
 {
@@ -31,14 +9,7 @@ int main(int argc, char *argv[])
 		std::cout << "Please give one, and only one, parameter as input\n";
 		return 0;
 	}
-	std::string type = getType(argv[1]);
-	std::cout << type << std::endl;
-
-
-	// std::stringstream literal(argv[1]);
-	// std::cout << "char: " << c << std::endl;
-	// std::cout << "int: " << i << std::endl;
-	// std::cout << "float: " << f << std::endl;
-	// std::cout << "double: " << d << std::endl;
+	Literal a(argv[1]);
+	a.printValues();
 	return 0;
 }
