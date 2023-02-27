8b1412f9cb0439a03d0006bcbcea4a11cadb37bf
Antonio Maria mouro ferreira abranches pinto <antoniomariabranches@gmail.com>
2023-02-27 17:20:40 +0000

    finished ex00 from module 07

--- Module_7/ex00/whatever.hpp
+++ Module_7/ex00/whatever.hpp
@@ -4,6 +4,29 @@
 # include <iostream>
 # include <string>
 
+template <class type>
+void swap(type &a, type &b)
+{
+	type c = b;
 
+	b = a;
+	a = c;
+}
+
+template <class type>
+type &min(type &a, type &b)
+{
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <class type>
+type &max(type &a, type &b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
 
 #endif
