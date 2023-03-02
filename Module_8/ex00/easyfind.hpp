d21fb271a3a3c668c003098f8ed4673912ae1577
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-03-02 19:50:46 +0000

    starting ex01 from module 8

--- Module_8/ex00/easyfind.hpp
+++ Module_8/ex00/easyfind.hpp
@@ -4,13 +4,20 @@
 # include <iostream>
 # include <string>
 # include <algorithm>
-// # include <list>
-// # include <map>
 
-template <class T>
-T &easyfind(T &container, int toFind)
+class CantFindElementException : public std::exception {
+	public: virtual const char* what() const throw() {
+		return "easyfind: Can not find element in container!";
+	}
+};
+
+template <typename T>
+typename T::iterator easyfind(T &container, int toFind)
 {
-	if (std::find(container.begin(), container.end(), item) != container.end())
+	typename T::iterator ptr = std::find(container.begin(), container.end(), toFind);
+	if (ptr == container.end())
+		throw CantFindElementException();
+	return ptr;
 	
 }
 
--- Module_8/ex00/main.cpp
+++ Module_8/ex00/main.cpp
@@ -1,12 +1,64 @@
 #include <easyfind.hpp>
 #include <vector>
+#include <list>
+#include <map>
+#include <iterator>
 
 int main()
 {
-	std::vector<int> vec;
+	{
+		std::cout << "____Testing 'easyfind' with vector____\n";
+		std::vector<int> vec;
+		std::vector<int>::iterator ptr;
 
-	for (int i = 1; i <= 5; i++)
-        vec.push_back(i);
-	::easyfind(vec, 0);
+		vec.push_back( 4 );
+		vec.push_back( 3 );
+		vec.push_back( 5 );
+		vec.push_back( 1 );
+		vec.push_back( 6 );
+		std::cout << "Created vector -> {";
+		for (std::size_t i = 0; i < vec.size(); i++)
+			std::cout << vec[i] << ",";
+		std::cout << "\b}\n";
+		std::cout << "Try to find element '0': ";
+		try
+		{
+			ptr = easyfind(vec, 0);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << "Try to find element '3': ";
+		ptr = easyfind(vec, 3);
+		std::cout << *ptr << std::endl;
+	}
+	{
+		std::cout << "____Testing 'easyfind' with list____\n";
+		std::list<int> lst;
+		std::list<int>::iterator ptr;
+
+		lst.push_back( 4 );
+		lst.push_back( 3 );
+		lst.push_back( 5 );
+		lst.push_back( 1 );
+		lst.push_back( 6 );
+		std::cout << "Created list -> (";
+		for( std::list<int>::iterator iter= lst.begin(); iter != lst.end(); iter++ )
+			std::cout << *iter << ",";
+		std::cout << "\b)\n";
+		std::cout << "Try to find element '-4': ";
+		try
+		{
+			ptr = easyfind(lst, -4);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << "Try to find element '5': ";
+		ptr = easyfind(lst, 5);
+		std::cout << *ptr << std::endl;	
+	}
 	return 0;
 }
