6d903628bdf7d09ecaa05359df4de2f1bb01c01c
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-03-20 18:00:20 +0000

    ex00 almost done, just needs some code cleaning

--- Module_9/ex00/main.cpp
+++ Module_9/ex00/main.cpp
@@ -1,9 +1,78 @@
 #include <BitcoinExchange.hpp>
-#include <filesystem>
+#include <sstream>
 
-int main()
+bool fileExists(const char *fileName)
 {
-	BitcoinExchange a("data.csv");
-	std::cout << a.valueOfBitcoin(1, "2011-04-02") << std::endl;
+	std::ifstream f(fileName);
+	return f.good();
+}
+
+int fterror(std::string message)
+{
+	std::cout << "Error: " << message << std::endl;
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2 || fileExists(argv[1]) == false)
+		return fterror("could not open file.");
+	BitcoinExchange BitcoinDataBase("data.csv");
+	std::ifstream file(argv[1]);
+	if (!file)
+		return fterror("could not open file.");
+	std::string line, word, date, value;
+	while (getline(file, line))
+	{
+		std::stringstream str(line);
+		int counter = 0;
+		while (getline(str, word, '|'))
+		{
+			word.erase(std::remove_if(word.begin(), word.end(), ::isspace), word.end());
+			if (!counter)
+				date = word;
+			else
+				value = word;
+			counter++;
+		}
+		if (counter < 2)
+			fterror("bad input => " + line);
+		else
+		{
+			float amount = atof(value.c_str());
+			if (amount < 0.0f)
+				fterror("not a positive number.");
+			else if (amount > 1000.0f)
+				fterror("too large a number.");
+			else
+			{
+				if (date == "date")
+					continue;
+				std::string datePart;
+				int year, month, day;
+				std::stringstream wrd(date);
+				counter = 0;
+				while(getline(wrd, datePart, '-'))
+				{
+					if (!counter)
+						year = atoi(datePart.c_str());
+					else if (counter == 1)
+						month = atoi(datePart.c_str());
+					else
+						day = atoi(datePart.c_str());
+					counter++;
+				}
+				if (year < 0 || year > 2023 || month < 0 || month > 11 || day < 0 || day > 31)
+					fterror("bad input => " + date);
+				else
+				{
+					std::cout << date << " => " << amount << " = ";
+					std::cout << BitcoinDataBase.valueOfBitcoin(amount, date) << std::endl;
+				}
+			}
+			
+		}
+	}
+	file.close();
 	return 0;
 }
