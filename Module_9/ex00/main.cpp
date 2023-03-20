391dcac771419747ece3c6b7860c6ecadcafe8a5
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-03-20 18:45:58 +0000

    ...

--- Module_9/ex00/main.cpp
+++ Module_9/ex00/main.cpp
@@ -13,6 +13,40 @@ int fterror(std::string message)
 	return 1;
 }
 
+int getDateAndValue(const std::string line, std::string &date, std::string &value)
+{
+	std::stringstream str(line);
+	std::string word;
+	int counter = 0;
+	while (getline(str, word, '|'))
+	{
+		word.erase(std::remove_if(word.begin(), word.end(), ::isspace), word.end());
+		if (!counter)
+			date = word;
+		else
+			value = word;
+		counter++;
+	}
+	return counter;
+}
+
+void divideDate(std::string date, int &year, int &month, int &day)
+{
+	std::string datePart;
+	std::stringstream wrd(date);
+	int counter = 0;
+	while(getline(wrd, datePart, '-'))
+	{
+		if (!counter)
+			year = atoi(datePart.c_str());
+		else if (counter == 1)
+			month = atoi(datePart.c_str());
+		else
+			day = atoi(datePart.c_str());
+		counter++;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 2 || fileExists(argv[1]) == false)
@@ -24,18 +58,7 @@ int main(int argc, char **argv)
 	std::string line, word, date, value;
 	while (getline(file, line))
 	{
-		std::stringstream str(line);
-		int counter = 0;
-		while (getline(str, word, '|'))
-		{
-			word.erase(std::remove_if(word.begin(), word.end(), ::isspace), word.end());
-			if (!counter)
-				date = word;
-			else
-				value = word;
-			counter++;
-		}
-		if (counter < 2)
+		if (getDateAndValue(line, date, value) < 2)
 			fterror("bad input => " + line);
 		else
 		{
@@ -48,20 +71,8 @@ int main(int argc, char **argv)
 			{
 				if (date == "date")
 					continue;
-				std::string datePart;
 				int year, month, day;
-				std::stringstream wrd(date);
-				counter = 0;
-				while(getline(wrd, datePart, '-'))
-				{
-					if (!counter)
-						year = atoi(datePart.c_str());
-					else if (counter == 1)
-						month = atoi(datePart.c_str());
-					else
-						day = atoi(datePart.c_str());
-					counter++;
-				}
+				divideDate(date, year, month, day);
 				if (year < 0 || year > 2023 || month < 0 || month > 11 || day < 0 || day > 31)
 					fterror("bad input => " + date);
 				else
