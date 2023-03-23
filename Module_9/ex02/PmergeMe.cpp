dbad4622a58bfbe4f70a0db2ec96f995a671b834
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-03-23 22:34:56 +0000

    finished module 09

--- Module_9/ex02/PmergeMe.cpp
+++ Module_9/ex02/PmergeMe.cpp
@@ -90,18 +90,165 @@ int PmergeMe::fterror(const std::string msg) const
 	throw PmergeMe::ErrorException();
 }
 
+// Sort functions for list
+
+std::list<unsigned int>::iterator get(std::list<unsigned int> &_list, int index)
+{
+	std::list<unsigned int>::iterator it = _list.begin();
+    for(int i = 0; i < index; i++)
+	{
+        it++;
+    }
+    return it;
+}
+
+std::list<std::list<unsigned int> >::iterator get(std::list<std::list<unsigned int> > &_list, int index)
+{
+	std::list<std::list<unsigned int> >::iterator it = _list.begin();
+    for(int i = 0; i < index; i++){
+        ++it;
+    }
+    return it;
+}
+
+void printList(std::list<unsigned int> &lst)
+{
+	std::list<unsigned int>::iterator it = lst.begin();
+	std::cout << "-- list size: " << (int)lst.size() << " --\n";
+    for(int i = 0; i < (int)lst.size(); i++)
+	{
+		std::cout << *it;
+		std::cout << ",";
+        it++;
+    }
+	std::cout << std::endl;
+}
+
+void printListOfList(std::list<std::list<unsigned int> > listOfSequences)
+{
+	std::cout << "------------------------------------------------------------\n";
+	std::cout << "-\n";
+	for (std::list<std::list<unsigned int> >::iterator it = listOfSequences.begin(); it != listOfSequences.end(); ++it)
+	{
+		std::cout << "[";
+		for (std::list<unsigned int>::iterator itScd = it->begin(); itScd != it->end(); ++itScd)
+		{
+			std::cout << *itScd;
+			std::cout << ",";
+		}
+		std::cout << "]\n";
+	}
+	std::cout << "-\n";
+}
+
+std::list<std::list<unsigned int> > &divideList(std::list<std::list<unsigned int> > &sqc, int k)
+{
+	if (k == 0)
+		return sqc;
+	if ((int)sqc.front().size() <= k)
+	{
+		sqc.push_back(sqc.front());
+		sqc.erase(sqc.begin());
+		return divideList(sqc, 0);
+	}
+	std::list<unsigned int> newList;
+	for (int i = 0; i < k; i++)
+		newList.push_back(*(get(sqc.front(), i)));
+	for (int i = 0; i < k; i++)
+		sqc.front().pop_front();
+	sqc.push_back(newList);
+	return divideList(sqc, k);
+}
+
+std::list<unsigned int> &insertionSortList(std::list<unsigned int> &sqc)
+{
+	std::list<unsigned int>::iterator next = sqc.begin();
+	next++;
+	std::list<unsigned int>::iterator mem;
+	for (std::list<unsigned int>::iterator current = sqc.begin(); current != sqc.end(); )
+	{
+		if (*current > *next)
+		{
+			std::swap(*current, *next);
+			if (current != sqc.begin())
+				next--;
+			if (current != sqc.begin())
+				current--;
+		}
+		else
+		{
+			next++;
+			current++;
+		}
+	}
+	return sqc;
+}
+
+std::list<std::list<unsigned int> > &mergeSortList(std::list<std::list<unsigned int> > &sqc)
+{
+	if ((int)sqc.size() > 2)
+	{
+		std::list<std::list<unsigned int> > newSqc;
+		for (int i = 0; i < 2; i++)
+		{
+			newSqc.push_back(sqc.front());
+			sqc.pop_front();
+		}
+		newSqc = mergeSortList(newSqc);
+		sqc.push_back(newSqc.front());
+		return mergeSortList(sqc);
+	}
+	if ((int)sqc.size() == 2)
+	{
+		std::list<unsigned int> sortedVector;
+		std::list<unsigned int> &firstLst = sqc.front();
+		std::list<unsigned int> &secondLst = *(get(sqc, 1));
+		while ((int)firstLst.size() > 0 && (int)secondLst.size() > 0)
+		{
+			if (firstLst.front() < secondLst.front())
+			{
+				sortedVector.push_back(firstLst.front());
+				firstLst.pop_front();
+			}
+			else
+			{
+				sortedVector.push_back(secondLst.front());
+				secondLst.pop_front();
+			}
+		}
+		for (int i = 0; i < (int)firstLst.size(); i++)
+			sortedVector.push_back((*(get(firstLst, i))));
+		for (int i = 0; i < (int)secondLst.size(); i++)
+			sortedVector.push_back((*(get(secondLst, i))));
+		for (int i = 0; i < 2; i++)
+			sqc.pop_front();
+		sqc.push_back(sortedVector);
+	}
+	return sqc;
+}
+
 std::string PmergeMe::sortList()
 {
 	clock_t start = clock();
 	// Clock started
-
-	// this->_lstSequence.sort();
+	std::list<unsigned int> lst = this->_lstSequence;
+	std::list<std::list<unsigned int> > lstOfSequences;
+	lstOfSequences.push_back(lst);
+	lstOfSequences = divideList(lstOfSequences, 2);
+	// printListOfList(lstOfSequences);
+	for (std::list<std::list<unsigned int> >::iterator it = lstOfSequences.begin(); it != lstOfSequences.end(); it++)
+		insertionSortList(*it);
+	// printListOfList(lstOfSequences);
+	lstOfSequences = mergeSortList(lstOfSequences);
+	this->_lstSequence = lstOfSequences.front();
 	// Clock stoped
 	clock_t end = clock();
 	std::string time =  SSTR("Time to process a range of " << this->_lstSequence.size() << " elements with std::list : " << float(end - start)/CLOCKS_PER_SEC << " sec\n");
 	return time;
 
 }
+
+// Sort functions for vector
 
 void printVectorOfVectors(std::vector<std::vector<unsigned int> > vectOfSequences)
 {
@@ -125,21 +272,16 @@ std::vector<std::vector<unsigned int> > &divideVector(std::vector<std::vector<un
 		return sqc;
 	if ((int)sqc[0].size() <= k)
 	{
-		std::vector<unsigned int> newVector = sqc[0];
+		sqc.push_back(sqc[0]);
 		sqc.erase(sqc.begin());
-		sqc.push_back(newVector);
 		return divideVector(sqc, 0);
 	}
-	if ((int)sqc[0].size() > k)
-	{
-		std::vector<unsigned int> newVector;
-		for (int i = 0; i < k; i++)
-			newVector.push_back(sqc[0][i]);
-		sqc[0].erase(sqc[0].begin(), sqc[0].begin() + k);
-		sqc.push_back(newVector);
-		return divideVector(sqc, k);
-	}
-	return sqc;
+	std::vector<unsigned int> newVector;
+	for (int i = 0; i < k; i++)
+		newVector.push_back(sqc[0][i]);
+	sqc[0].erase(sqc[0].begin(), sqc[0].begin() + k);
+	sqc.push_back(newVector);
+	return divideVector(sqc, k);
 }
 
 std::vector<unsigned int> &insertionSort(std::vector<unsigned int> &sqc)
@@ -200,13 +342,13 @@ std::string PmergeMe::sortVector()
 	clock_t start = clock();
 	// Clock started
 	std::vector<unsigned int> vec = this->_vctSequence;
-	//  divide the given N elements of array into (N/K) groups of each group of size K
 	std::vector<std::vector<unsigned int> > vectOfSequences;
 	vectOfSequences.push_back(vec);
-	vectOfSequences = divideVector(vectOfSequences, 2);
+	vectOfSequences = divideVector(vectOfSequences, 190);
 	for (int i = 0;  i < (int)vectOfSequences.size(); i++)
 		insertionSort(vectOfSequences[i]);
 	vectOfSequences = mergeSort(vectOfSequences);
+	this->_vctSequence = vectOfSequences[0];
 	// Clock stoped
 	clock_t end = clock();
 	std::string time =  SSTR("Time to process a range of " << this->_vctSequence.size() << " elements with std::vector : " << float(end - start)/CLOCKS_PER_SEC << " sec\n");
